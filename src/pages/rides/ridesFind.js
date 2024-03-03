fd5585905ec9bd6a78b2e7ee3af326337a1c3eaf
Amber Lim <amberlim1004@gmail.com>
2024-03-03 15:35:57 -0800

    fixed time/date, added a find and create button and linked to respective pages, and changed font/spacing

---
+++ src/pages/rides/ridesFind.js
@@ -0,0 +1,13 @@
+// Filename - pages/profile/ridesFind.js
+
+import React from "react";
+
+const ridesFind = () => {
+	return (
+		<div>
+			<h1>find a ride!</h1>
+		</div>
+	);
+};
+
+export default ridesFind;
