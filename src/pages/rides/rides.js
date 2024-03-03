5105291aa4d7cf8471ac0a6d84f7467a0618852a
Amber Lim <amberlim1004@gmail.com>
2024-03-03 00:58:16 -0800

    working on calendar and formatting

--- src/pages/rides/rides.js
+++ src/pages/rides/rides.js
@@ -1,16 +1,21 @@
 // Filename - pages/rides/rides.js
 import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import styled from "styled-components";
 
 const Rides = () => {
+
+  // to and from buttons
+
   const [fromLocation, setFromLocation] = useState("lax");
   const [toLocation, setToLocation] = useState("ucla");
-
+  // for from dropdown, handles options
   const handleFromChange = (e) => {
     const selectedFromLocation = e.target.value;
     setFromLocation(selectedFromLocation);
     setToLocation(selectedFromLocation === "ucla" ? "lax" : "ucla");
   };
-
+  // for to dropdown, handles options
   const handleToChange = (e) => {
     const selectedToLocation = e.target.value;
     setToLocation(selectedToLocation);
@@ -19,11 +24,19 @@ const Rides = () => {
 
   const locationOptions = ["ucla", "lax"].sort();
 
+  // date and time picker button
+  const currentDateTime = new Date();
+  const [selectedDate, setSelectedDate] = useState(currentDateTime);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
   return (
     <div>
       <h1>Need a Ride?</h1>
 
-      {/* "From" dropdown */}
+      {/* from dropdown */}
       <label htmlFor="fromLocation">From:</label>
       <select id="fromLocation" value={fromLocation} onChange={handleFromChange}>
         {locationOptions.map((option) => (
@@ -33,7 +46,7 @@ const Rides = () => {
         ))}
       </select>
 
-      {/* "To" dropdown */}
+      {/* to dropdown */}
       <label htmlFor="toLocation">To:</label>
       <select id="toLocation" value={toLocation} onChange={handleToChange}>
         {locationOptions.map((option) => (
@@ -43,7 +56,16 @@ const Rides = () => {
         ))}
       </select>
 
-      {/* Add other content as needed */}
+      {/* Date and Time Picker */}
+      <div>
+        <DatePicker
+          selected={selectedDate}
+          onChange={handleDateChange}
+          showTimeSelect
+          dateFormat="Pp"
+          minDate={currentDateTime}
+        />
+      </div>
     </div>
   );
 };
