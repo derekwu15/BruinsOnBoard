9d558528ede3c8ca89f3caa7e452dd98e3b0623e
jywhumphreys <gimli398@gmail.com>
2024-03-02 23:35:15 -0800

    resolving merge conflicts

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import {
 	Route,
 } from "react-router-dom";
 import Home from "./pages/home/home";
-import About from "./pages/about/about";
 import Login from "./pages/profile/login";
 import SignUp from "./pages/profile/signup";
 import Rides from "./pages/rides/rides";
@@ -19,17 +18,9 @@ function App() {
 			<Navbar />
 			<Routes>
 				<Route exact path="/" element={<Home />} />
-				<Route path="/about" element={<About />} />
-<<<<<<< HEAD
-				<Route
-					path="/sign-up"
-					element={<SignUp />}
-				/>
 				<Route path="/rides" element={<Rides />} />
-=======
 				<Route path="/login" element={<Login />} />
               	<Route path="/signup" element={<SignUp />} />
->>>>>>> d669e55408ec85529f29b34f4fdd6d81348fa040
 			</Routes>
 		</Router>
 	);
