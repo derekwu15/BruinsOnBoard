4b763b03100f9826fce19e2a0fd70f57dff453fb
jywhumphreys <gimli398@gmail.com>
2024-03-02 16:26:40 -0800

    start app and server together

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+app.get('/', (req, res) => {
+  res.json({mssg: 'welcome'})
+})
