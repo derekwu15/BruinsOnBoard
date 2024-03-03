baa0878eefdd6cb8ec2c91e036525218fcf7c053
Amber Lim <amberlim1004@gmail.com>
2024-03-03 15:35:57 -0800

    fixed time/date, added a find and create button and linked to respective pages, and changed font/spacing

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import Home from "./pages/home/home";
 import Login from "./pages/profile/login";
 import SignUp from "./pages/profile/signup";
 import Rides from "./pages/rides/rides";
+import RidesCreate from "./pages/rides/ridesCreate";
+import RidesFind from "./pages/rides/ridesFind";
 
 function App() {
 	return (
@@ -21,6 +23,8 @@ function App() {
 				<Route path="/rides" element={<Rides />} />
 				<Route path="/login" element={<Login />} />
               	<Route path="/signup" element={<SignUp />} />
+				<Route path="/ridesCreate" element={<RidesCreate />} />
+				<Route path="/ridesFind" element={<RidesFind />} />
 			</Routes>
 		</Router>
 	);
