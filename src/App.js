51d6391be88d42a0ac575358f14d910f93a67985
Amber Lim <amberlim1004@gmail.com>
2024-03-09 15:30:00 -0800

    fixed ride routing, added calendar, made popup for create a ride

--- src/App.js
+++ src/App.js
@@ -10,9 +10,7 @@ import {
 import Home from "./pages/home/home";
 import Login from "./pages/profile/login";
 import SignUp from "./pages/profile/signup";
-import Rides from "./pages/rides/rides";
 import Messages from "./pages/messages/message";
-import RidesCreate from "./pages/rides/ridesCreate";
 import RidesFind from "./pages/rides/ridesFind";
 
 function App() {
@@ -21,11 +19,9 @@ function App() {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/rides" element={<Rides />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/message" element={<Messages />} />
-        <Route path="/ridesCreate" element={<RidesCreate />} />
         <Route path="/ridesFind" element={<RidesFind />} />
       </Routes>
     </Router>
