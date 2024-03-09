13bc19202c1b5713304e85d8f7cfb66a77e0637c
jywhumphreys <gimli398@gmail.com>
2024-03-09 08:17:32 -0800

    added signup error message

--- backend/models/profileModel.js
+++ backend/models/profileModel.js
@@ -8,12 +8,12 @@ const profileSchema = new Schema({
   name: {
     type: String,
     required: true,
-    unique: true,
     trim: true,
     minlength: 3
   },
   email: {
     type: String,
+    unique: true,
     required: true
   },
   password: {
