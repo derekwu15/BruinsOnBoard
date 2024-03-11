ae60ba37263b13f5b6c74d9ab886f830d1673fc2
Amber Lim <amberlim1004@gmail.com>
2024-03-10 18:08:42 -0700

    added profile dropdown with your profile and log out button

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { NavLink as Link } from "react-router-dom";
 import {useLogout} from '../../hooks/useLogout'
@@ -55,28 +55,6 @@ const LoginButton = styled(NavLink)`
   }
 `;
 
-const LogOutButton = styled(NavLink)`
-  display: inline-block;
-  background: #ffffff;
-  color: #000000;
-  font-size: 16px;
-  font-weight: 500;
-  text-decoration: none;
-  padding: 10px 20px;
-  width: 70px;
-  height: 15px;
-  cursor: pointer;
-  transition: background 0.3s ease, color 0.3s ease;
-  border-radius: 20px;
-  text-align: center;
-  line-height: 15px;
-  &:hover,
-  &.active {
-    background: #ffc52d;
-    color: #000000;
-  }
-`;
-
 const NavMenu = styled.div`
   display: flex;
   align-items: center;
@@ -88,12 +66,67 @@ const NavMenu = styled.div`
   }
 `;
 
+const DropdownMenu = styled.div`
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  background: #0172c0;
+
+  min-width: 160px;
+  padding: 12px 16px;
+  z-index: 1;
+  right: 0;
+`;
+
+const ProfileButton = styled.div`
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: 500;
+  text-decoration: none;
+  padding: 10px 20px;
+  transition: color 0.3s ease;
+  
+  cursor: pointer;
+  position: relative;
+  &:hover ${DropdownMenu} {
+    display: block;
+  }
+  &:hover {
+    color: #ffc52d;
+  }
+  &.active {
+    color: #ffc52d;
+  }
+`;
+
+const DropdownLink = styled(Link)`
+  color: #ffffff;
+  font-size: 16px;
+  font-weight: 500;
+  text-decoration: none;
+  padding: 10px 20px;
+  height: 100%;
+  cursor: pointer;
+  display: block;
+
+  &:hover {
+    background-color: #0056b3;
+  }
+
+  &:hover {
+    color: #ffc52d;
+  }
+`;
+
 const Navbar = () => {
   const {logout} = useLogout()
   const { user } = useAuthContext()
-  const handleClick = () => {
-    logout()
-  }
+  const [dropdown, setDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdown(!dropdown);
+  };
+
   return (
     <>
       <Nav>
@@ -106,11 +139,19 @@ const Navbar = () => {
           </NavLink>
         </NavMenu>
         {user && (
-          <div>
-            <span>{user.email}</span>
-            <LogOutButton onClick={handleClick}>Log out</LogOutButton>
-          </div>
+          <ProfileButton onClick={toggleDropdown}>
+            Profile
+            <DropdownMenu>
+              <DropdownLink to="/profile" activeStyle>
+                Your Profile
+              </DropdownLink>
+              <DropdownLink as="LogoutButton" onClick={() => logout()} activeStyle>
+                Log out
+              </DropdownLink> 
+            </DropdownMenu>
+          </ProfileButton>
         )}
+
         {!user && (
           <LoginButton to="/login" activeStyle>
             Login
